ea0b18e4d795a22484352913107ca5ed43ae6b75
Salim Zidane <salim.zidane44@hotmail.com>
2026-04-07 18:55:49 +0200

    feat: support for more currencies

--- src/components/CurrencyButton.jsx
+++ src/components/CurrencyButton.jsx
@@ -2,6 +2,41 @@ import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@
 import {Input} from "@/components/ui/input.jsx";
 
 function CurrencyButton({ readOnly, onChange, onCurrencyChange, result, currency }) {
+
+    const currencies = [
+        { code: "AUD", label: "Australian Dollar" },
+        { code: "BRL", label: "Brazilian Real" },
+        { code: "CAD", label: "Canadian Dollar" },
+        { code: "CHF", label: "Swiss Franc" },
+        { code: "CNY", label: "Chinese Renminbi Yuan" },
+        { code: "CZK", label: "Czech Koruna" },
+        { code: "DKK", label: "Danish Krone" },
+        { code: "EUR", label: "Euro" },
+        { code: "GBP", label: "British Pound" },
+        { code: "HKD", label: "Hong Kong Dollar" },
+        { code: "HUF", label: "Hungarian Forint" },
+        { code: "IDR", label: "Indonesian Rupiah" },
+        { code: "ILS", label: "Israeli New Shekel" },
+        { code: "INR", label: "Indian Rupee" },
+        { code: "ISK", label: "Icelandic Króna" },
+        { code: "JPY", label: "Japanese Yen" },
+        { code: "KRW", label: "South Korean Won" },
+        { code: "MXN", label: "Mexican Peso" },
+        { code: "MYR", label: "Malaysian Ringgit" },
+        { code: "NOK", label: "Norwegian Krone" },
+        { code: "NZD", label: "New Zealand Dollar" },
+        { code: "PHP", label: "Philippine Peso" },
+        { code: "PLN", label: "Polish Złoty" },
+        { code: "RON", label: "Romanian Leu" },
+        { code: "SEK", label: "Swedish Krona" },
+        { code: "SGD", label: "Singapore Dollar" },
+        { code: "THB", label: "Thai Baht" },
+        { code: "TRY", label: "Turkish Lira" },
+        { code: "USD", label: "United States Dollar" },
+        { code: "ZAR", label: "South African Rand" },
+    ]
+
+
     return (
         <>
             <div className="flex flex-row gap-5 items-center justify-center w-full">
@@ -13,13 +48,15 @@ function CurrencyButton({ readOnly, onChange, onCurrencyChange, result, currency
                         <Input onChange={(e) => onChange(e.target.value)} className="bg-white text-black" placeholder="Input Value..."/>
                 }
                 <Select value={currency} onValueChange={onCurrencyChange}>
-                    <SelectTrigger className="bg-white text-black w-26">
+                    <SelectTrigger className="bg-white text-black w-60">
                         <SelectValue />
                     </SelectTrigger>
                     <SelectContent>
-                        <SelectItem value="USD">$ USD</SelectItem>
-                        <SelectItem value="EUR">€ EUR</SelectItem>
-                        <SelectItem value="GBP">£ GBP</SelectItem>
+                        <SelectContent>
+                            {currencies.map((c) => (
+                                <SelectItem key={c.code} value={c.code}>{c.label}</SelectItem>
+                            ))}
+                        </SelectContent>
                     </SelectContent>
                 </Select>
             </div>
