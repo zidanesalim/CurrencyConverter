7c9736de0f62a871635eaaca1285f270ebe86729
Salim Zidane <salim.zidane44@hotmail.com>
2026-04-10 21:59:44 +0200

    chore: updated the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,17 @@
+import { SiGithub } from '@icons-pack/react-simple-icons'
+
 function Footer() {
     return (
-        <div className="flex flex-col justify-center items-center pt-10">
+        <div className="flex flex-col items-center pt-10">
             <div className="w-3xl h-0.5 bg-white/20"></div>
-            <div className="flex flex-row">
-                <span className="py-2 text-white font-inter">© 2026 Salim Zidane. All rights reserved.</span>
-                <div className="flex flex-row">
+            <div className="w-3xl flex flex-row justify-between items-center py-2 px-4">
+                <span className="text-white/60 text-xs">© 2026 Salim Zidane. All rights reserved.</span>
+                <div className="flex flex-row gap-6">
+                    <a href="https://github.com/zidanesalim" className="text-white/60 hover:text-white transition-colors">
+                        <SiGithub size={18} />
+                    </a>
                 </div>
+
             </div>
         </div>
     )
